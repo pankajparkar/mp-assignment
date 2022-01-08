f9d07c9cb3f9f2503068d8837db04d30607fa469
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-01-09 02:33:40 +0800

    Added search filter

--- src/app/activity1/list/list.component.ts
+++ src/app/activity1/list/list.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { BehaviorSubject, combineLatest, map } from 'rxjs';
-import { BakedGood } from 'src/app/models/baked-goods.model';
 import { BakedGoodsService } from '../../services/baked-goods.service';
 
 interface BakedGoodSorting {
   id?: boolean;
   name?: boolean;
   type?: boolean;
+  search: string;
 }
 
 @Component({
@@ -18,14 +18,22 @@ interface BakedGoodSorting {
 export class BakedGoodListComponent implements OnInit {
 
   sorting = {} as BakedGoodSorting;
-  
   private sorting$ = new BehaviorSubject(this.sorting);
-  @ViewChild('form', {static: true}) form!: NgForm; 
-
+  ids = this.bakedGoodsService.getUniques('id');
+  types = this.bakedGoodsService.getUniques('type');
+  batters = this.bakedGoodsService.getUniques('name');
   backedGood$ = combineLatest(this.bakedGoodsService.bakedGoods$, this.sorting$).pipe(
     map(([bakedGoods, sorting]) => {
-      const { id, name, type} = sorting;
-      return bakedGoods.sort((a: any, b: any) => {
+      const { id, name, type, search} = sorting;
+      const filteredResult = search ? bakedGoods.filter(
+        bakedGood => (
+          (bakedGood.id).toString().includes(search) || 
+          (bakedGood.type).includes(search) || 
+          (bakedGood.name).includes(search) || 
+          (bakedGood.topping).includes(search)
+        )
+      ): bakedGoods;
+      return [...filteredResult].sort((a: any, b: any) => {
         return (
           (!id || (b.id - a.id)) ||
           (!type && a.type.localeCompare(b.type)) ||
@@ -34,9 +42,7 @@ export class BakedGoodListComponent implements OnInit {
       });
     })
   );
-  ids = this.bakedGoodsService.getUniques('id');
-  types = this.bakedGoodsService.getUniques('type');
-  batters = this.bakedGoodsService.getUniques('name');
+  @ViewChild('form', {static: true}) form!: NgForm; 
 
   constructor(
     private bakedGoodsService: BakedGoodsService
