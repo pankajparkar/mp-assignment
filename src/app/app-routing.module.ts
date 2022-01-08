aeb4dfef4500c071b6de2d8ed5d27d3bc2d97e93
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-01-09 00:13:04 +0800

    Update navbar title based on selected route page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,27 @@ import { Activity2Component } from './activity2/activity2.component';
 import { Activity3Component } from './activity3/activity3.component';
 
 const routes: Routes = [
-  {path: 'activity1', component: Activity1Component},
-  {path: 'activity2', component: Activity2Component},
-  {path: 'activity3', component: Activity3Component},
+  {
+    path: 'activity1',
+    component: Activity1Component,
+    data: {
+      title: 'Activity 1'
+    },
+  },
+  {
+    path: 'activity2',
+    component: Activity2Component,
+    data: {
+      title: 'Activity 2'
+    },
+  },
+  {
+    path: 'activity3', 
+    component: Activity3Component,
+    data: {
+      title: 'Activity 3'
+    },
+  },
   {path: '**', redirectTo: 'activity1'},
 ];
 
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+
+const DEFAULT_TITLE = 'Application';
 
 @Component({
   selector: 'mp-navbar',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  title = DEFAULT_TITLE;
+
+  constructor(
+    readonly router: Router,
+    readonly route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe(
+      (event) => {
+        if (event instanceof NavigationEnd) {
+          const [currentRoute] = this.route.children;
+          if (currentRoute) {
+            // TODO: below has to be change if requirement changes
+            this.title = (currentRoute.routeConfig?.data as any)?.title || DEFAULT_TITLE;
+          }
+        }
+      }
+    );
   }
 
 }
