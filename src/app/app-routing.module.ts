d7c401ec0f2649076f0cd1d6c2b8356e2817fe90
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-01-08 23:44:53 +0800

    Added router configuration.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Activity1Component } from './activity1/activity1.component';
+import { Activity2Component } from './activity2/activity2.component';
+import { Activity3Component } from './activity3/activity3.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'activity1', component: Activity1Component},
+  {path: 'activity2', component: Activity2Component},
+  {path: 'activity3', component: Activity3Component},
+  {path: '**', redirectTo: 'activity1'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
